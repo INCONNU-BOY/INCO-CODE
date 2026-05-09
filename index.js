40134b6b95d60e4c53cade8e663766bf4115774f
INCONNU BOY <inconnuboy39@gmail.com>
2026-05-09 12:44:05 -0300

    INCONNU BOY TECH 🍂

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * INCO CODE — Mobile IDE
+ * @author INCO BOY TECH
+ * @version 1.0.0
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
